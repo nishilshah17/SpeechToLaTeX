dbed3686cbcff90cb56ed588168ef3d355514e45
RamV13 <ramsai.vellanki@gmail.com>
2014-09-13 02:35:20 -0400

    Slashes

--- js/text.js
+++ js/text.js
@@ -2,17 +2,17 @@ function convertText(text) {
 
     var convertedText;
 
-    convertedText = "\documentclass{article}" +
+    convertedText = "\\documentclass{article}" +
         "\\begin{document}" +
         "\\title{MCLA Concise Review}" +
-        "\author{Ram Vellanki, Manoaj Kandiakounder, Pranav Marupudi, Jintao Hang}" +
-        "\date{June 2014}" +
+        "\\author{Ram Vellanki, Manoaj Kandiakounder, Pranav Marupudi, Jintao Hang}" +
+        "\\date{June 2014}" +
         "\maketitle" +
         "\\tableofcontents" +
         "%MC CHAPTER 1.................................................... $"+
         "\\newpage" +
-        "\section{Parametrics and Polar Coordinates}" +
-        "\end{document}"
+        "\\section{Parametrics and Polar Coordinates}" +
+        "\\end{document}"
         ;
 
     compileLaTeX(convertedText);
