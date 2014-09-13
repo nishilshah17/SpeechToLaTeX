e9d176917035cce286ad4d6472f8bf7ff6e8c116
Nishil Shah <nishil.shah17@gmail.com>
2014-09-13 01:44:53 -0400

    ajax call to php

--- js/compile.js
+++ js/compile.js
@@ -1,7 +1,17 @@
 function compileLaTeX(latex_code) {
 
-//make call to php file
-
-//embed pdf
-document.getElementById('pdf').innnerHTML = '<iframe src="http://latex.aslushnikov.com/compile?url=https://raw.githubusercontent.com/aslushnikov/latex-online/master/sample/sample.tex" style="width:718px; height:700px;" frameborder="0"></iframe>';
+  //make call to php file
+  $.ajax({
+    url: '../tex/createFile.php',
+    data: { 'latex_code': latex_code },
+    success: function(data) {
+      alert(data);
+    }
+    error: function(error) {
+      alert(error);
+    }
+  });
+  
+  //embed pdf
+  document.getElementById('pdf').innnerHTML = '<iframe src="http://latex.aslushnikov.com/compile?url=https://raw.githubusercontent.com/aslushnikov/latex-online/master/sample/sample.tex" style="width:718px; height:700px;" frameborder="0"></iframe>';
 }
