7640967beda023bf00f21c1fff6369f3a0da493e
Nishil Shah <nishil.shah17@gmail.com>
2014-09-13 02:50:35 -0400

    remove echo

--- tex/createFile.php
+++ tex/createFile.php
@@ -1,12 +1,13 @@
 <?php
   $latex_code = $_POST['latex'];
-  echo $latex_code;
+
   $date = date_create();
   $unix = date_timestamp_get($date);
 
   $filename = $unix.".tex";
-  echo $filename;
+
   $newfile = fopen($filename, "w") or die("Unable to open file!");
   fwrite($newfile, $latex_code);
   fclose($newfile);
+  echo $filename;
 ?>
