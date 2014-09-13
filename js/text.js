f5c84f711c0e20a7c1e7ee73ff88f14826882d00
RamV13 <ramsai.vellanki@gmail.com>
2014-09-13 13:58:18 -0400

    Latex Conversion

--- js/text.js
+++ js/text.js
@@ -1,5 +1,6 @@
-
 var inEquation = false;
+var inSub = false;
+var inAbsolute = false;
 
 function countSpaces(s) {
     
@@ -23,8 +24,22 @@ function isVariable(s) {
     if (s.length == 1 && s.match(/^[a-zA-Z]/))
         return true;
     
-    if (s == "ex" || s == "why")
-        return true;
+    switch (s) {
+        case "ex": return true;
+        case "why": return true;
+        case "alpha": return true;
+        case "beta": return true;
+        case "gamma": return true;
+        case "delta": return true;
+        case "epsilon": return true;
+        case "zeta": return true;
+        case "eta": return true;
+        case "theta": return true;
+        case "iota": return true;
+        case "kappa": return true;
+        case "lambda": return true;
+        case "mu": return true;
+    }
     
     return false;
     
@@ -39,6 +54,30 @@ function convertVariable(s) {
     switch (s) {
         case "ex": return "x";
         case "why": return "y";
+        case "alpha": return "\alpha";
+        case "beta": return "\beta";
+        case "gamma": return "\gamma";
+        case "delta": return "\delta";
+        case "epsilon": return "\epsilon";
+        case "zeta": return "\zeta";
+        case "eta": return "\eta";
+        case "theta": return "\theta";
+        case "iota": return "\iota";
+        case "kappa": return "\kappa";
+        case "lambda": return "\lambda";
+        case "mu": return "\mu";
+        case "nu": return "\nu";
+        //case "xi": return "\xi";
+        case "omicron": return "\omicron";
+        case "pi": return "\pi";
+        case "rho": return "\rho";
+        case "sigma": return "\sigma";
+        case "tau": return "\tau";
+        //case "upsilon": return "\upsilon";
+        case "phi": return "\phi";
+        case "chi": return "\chi";
+        case "psi": return "\psi";
+        case "omega": return "\omega";
     }
     
 }
@@ -53,29 +92,42 @@ function isOperator(s) {
         case "times": return true;
         case "multiplied": return true;
         case "over": return true;
-        case "dived": return true;
+        case "divided": return true;
         case "by": return true;
+        case "power": return true;
         case "factorial": return true;
+        case "integral": return true;
+        case "derivative": return true;
     }
 }
 
 function convertOperator(s) {
     switch (s) {
         case "equals": return "=";
+        case "+": return "+";
         case "plus": return "+";
         case "added": return "+";
+        case "-": return "-";
         case "minus": return "-";
         case "subtracted": return "-";
-        case "times": return "*";
-        case "Times": return "*";
+        case "x": return "";
+        case "times": return "";
         case "multiplied": return "*";
-        case "over": return "/";
-        case "divided": return "/";
-        case "by": return "/";    
+        case "over": return "/";                                // TODO
+        case "divided": return "/";                             // TODO
+        case "by": return "/";                                  // TODO
+        case "factorial": return "!";
+        case "integral": return "\integral";
+        case "derivative": return "";                           // TODO
+        case "dot": return "\cdot";
+        case "cross": return "\times";
+        case "absolutevalue":   if (inEquation) return "\left|";
+                                else return "$\left|";
     }
 }
 
-function isCommand(s) {
+function
+isCommand(s) {
     switch (s) {
         case "compile": return true;
         case "newline": return true;
@@ -89,47 +141,103 @@ function isCommand(s) {
         case "italicized": return true;
         case "bold": return true;
         case "bolded": return true;
+        case "sub": return true;
     }
 }
 
 function convertCommand(s) {
+    
+    if (inEquation) {
+        switch (s) {
+            case "endequation": inEquation = false;
+                                return "$";
+            case "andequation": inEquation = false;
+                                return "$";
+        }
+    }
+    
+    if (inAbsolute) {
+        switch(s) {
+            case "end": inAbsolute = false;
+                        if (inEquation) return "\right|";
+                        else return "\right|$";
+            case "and": inAbsolute = false;
+                        if (inEquation) return "\right|";
+                        else return "\right|$";
+        }
+    }
+    
     switch(s) {
         case "newline": return "\\newline";
         case "newpage": return "\\newpage";
         case "newequation": inEquation = true;
                             return "$";
-        case "endequation": inEquation = false;
-                            return "$";
-        case "andequation": inEquation = false;
-                            return "$";
+        case "italics": return "\textit";
+        case "italicized": return "\textit";
+        case "bold": return "\textbf";
+        case "bolded": return "\textbf";
+        case "sub": inSub = true;
+                    return "_{";
     }
 }
 
 function convert(s) {
  
-    if (isNumber(s)) {
-        return s+" ";
-    } else if (isVariable(s)) {
-        return convertVariable(s)+" ";
-    } else if (inEquation && isOperator(s)) {
-        return convertOperator(s)+" ";
-    } else if (isCommand(s)) {
-        return convertCommand(s);
-    } else {
-        return s;
+    var extension = "";
+    
+    if (inSub) {
+        extension = "}";
+        inSub = false;
     }
     
+    if (!inEquation) {
+        extension+=" ";
+    }
+    
+    if (isNumber(s)) {
+        return s+extension;
+    } else if (inEquation && isVariable(s.toLowerCase())) {
+        return convertVariable(s.toLowerCase())+extension;
+    } else if (inEquation && isOperator(s.toLowerCase())) {
+        return convertOperator(s.toLowerCase())+extension;
+    } else if (isCommand(s.toLowerCase())) {
+        return convertCommand(s.toLowerCase());
+    } else {
+        return s+extension;
+    }
+    
+}
+
+function isReserved(s) {
+    switch(s) {
+        case "line": return true;
+        case "equation": return true;
+        case "page": return true;
+        case "italics": return true;
+        case "italicized": return true;
+        case "bold": return true;
+        case "bolded": return true;
+        case "value": return true;
+        // UPDATE RESERVED WORDS                                            TODO
+    }
+    return false;
+}
+
+function resetInputTypes() {
+    inEquation = false;
+    inSub = false;
 }
 
 function convertText(text) {
     
     var words = [];
     
+    // ADD TITLE/AUTHOR/DATE                                                TODO
     var convertedText = "\\\documentclass{article}" +
         "\\begin{document}" +
-        "\\title{MCLA Concise Review}" +
-        "\\\author{Ram Vellanki, Manoaj Kandiakounder, Pranav Marupudi, Jintao Hang}" +
-        "\\date{June 2014}" +
+        "\\title{(Title)}" +
+        "\\\author{(Author)}" +
+        "\\date{(Date)}" +
         "\\maketitle" +
         "\\tableofcontents" +
         "\\newpage" +
@@ -142,7 +250,8 @@ function convertText(text) {
         
         var append = false;
         
-        if (tmp == "new" || tmp == "end" || tmp == "and")
+        // keywords
+        if (tmp == "new" || tmp == "end" || tmp == "and" || tmp == "absolute")
             append = true;
         
         var tmp = "";
@@ -150,32 +259,20 @@ function convertText(text) {
             tmp = text;
         else
             tmp = [text.substring(0,text.indexOf(" "))];
-        if (append)
+        if (append && isReserved(tmp))
             words[i-1] = words[i-1]+tmp;
         else
             words = words.concat(tmp);
         text = text.substring(text.indexOf(" ")+1);
     }
     
-    inEquation = false;
+    resetInputTypes();
     
     for (var i = 0; i < words.length; i++) {
         convertedText+=convert(words[i]);
     }
     
     convertedText+="\\end{document}";
-        
-        /*"\\\documentclass{article}" +
-        "\\begin{document}" +
-        "\\title{MCLA Concise Review}" +
-        "\\\author{Ram Vellanki, Manoaj Kandiakounder, Pranav Marupudi, Jintao Hang}" +
-        "\\date{June 2014}" +
-        "\\maketitle" +
-        "\\tableofcontents" +
-        "\\newpage" +
-        "\\section{Parametrics and Polar Coordinates}" +
-        "\\end{document}"
-        ;*/
 
     compileLaTeX(convertedText);
 
