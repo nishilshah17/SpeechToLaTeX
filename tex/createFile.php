0b81bcf60bbb9dc70d6eeb764db0ba58de6dd324
Nishil Shah <nishil.shah17@gmail.com>
2014-09-13 02:00:34 -0400

    bug fix

--- tex/createFile.php
+++ tex/createFile.php
@@ -1,8 +1,10 @@
-$latex_code = $_POST['latex_code'];
+<?php
+  $latex_code = $_POST['latex_code'];
 
-$date = date_create();
-$unix = date_timestamp_get($date);
+  $date = date_create();
+  $unix = date_timestamp_get($date);
 
-$newfile = fopen($unix.".tex", "w") or die("Unable to open file!");
-fwrite($newfile, $latex_code);
-fclose($newfile);
+  $newfile = fopen($unix.".tex", "w") or die("Unable to open file!");
+  fwrite($newfile, $latex_code);
+  fclose($newfile);
+?>
