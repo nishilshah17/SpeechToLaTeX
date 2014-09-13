796c8750f15c047f422aeeb763502a3cce8af3c3
Nishil Shah <nishil.shah17@gmail.com>
2014-09-13 02:49:28 -0400

    latex_code

--- js/compile.js
+++ js/compile.js
@@ -6,7 +6,7 @@ function compileLaTeX(latex_code) {
   $.ajax({
     url: 'http://ramvellanki.com/s2l/tex/createFile.php',
     type: 'POST',
-    data: { latex: 'fjdsafdsa' },
+    data: { latex: latex_code },
     success: function(data) {
       alert(data);
       $('#pdf').append("<iframe src='http://latex.aslushnikov.com/compile?url=http://ramvellanki.com/s2l/tex/"+data+"' style='width:718px; height:700px;' frameborder='0'></iframe>")
