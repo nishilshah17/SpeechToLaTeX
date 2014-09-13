75a55603366c9467353b3401c0678c983020299e
Nishil Shah <nishil.shah17@gmail.com>
2014-09-13 04:20:46 -0400

    speech.js

--- js/compile.js
+++ js/compile.js
@@ -1,15 +1,14 @@
 function compileLaTeX(latex_code) {
 
   //make call to php file
-  alert(latex_code);
-
   $.ajax({
     url: 'http://ramvellanki.com/s2l/tex/createFile.php',
     type: 'POST',
     data: { latex: latex_code },
     success: function(data) {
-      alert(data);
+      //compile to LaTeX using this API
       $('#pdf').append("<iframe src='http://latex.aslushnikov.com/compile?url=http://ramvellanki.com/s2l/tex/"+data+"' style='width:718px; height:700px;' frameborder='0'></iframe>")
+      //embed PDF
     },
     error: function(error) {
       alert(error);
--- js/text.js
+++ js/text.js
@@ -1,20 +1,20 @@
 function countSpaces(s) {
-    
+
     var count = 0;
-    
+
     for (var i = 0; i < s.length; i++) {
         if (s.charAt(i) == " ")
             count++;
     }
-    
+
     return count;
-    
+
 }
 
 function convertText(text) {
 
     text = "x plus y equals three";
-    
+
     var words = [];
 
     for (var i = 0; i < countSpaces(text); i++) {
@@ -22,7 +22,7 @@ function convertText(text) {
         words = words.concat(tmp);
         text = text.substring(text.indexOf(" ")+1);
     }
-    
+
     var convertedText;
 
     convertedText = "\\\documentclass{article}" +
