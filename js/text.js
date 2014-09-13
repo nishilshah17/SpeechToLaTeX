91db2b580b13519c7182f6dc088544560ccdc820
RamV13 <ramsai.vellanki@gmail.com>
2014-09-13 00:59:09 -0400

    First Test

---
+++ js/text.js
@@ -0,0 +1,19 @@
+function convertText(text) {
+    
+    var convertedText;
+    
+    convertedText = "\documentclass{article}" +
+        "\begin{document}" +
+        "\title{MCLA Concise Review}" +
+        "\author{Ram Vellanki, Manoaj Kandiakounder, Pranav Marupudi, Jintao Hang}" +
+        "\date{June 2014}" +
+        "\maketitle" +
+        "\tableofcontents" +
+        "%MC CHAPTER 1.................................................... $"+
+        "\newpage" +
+        "\section{Parametrics and Polar Coordinates}"
+        ;
+    
+    compileLaTeX(convertedText);
+    
+}
