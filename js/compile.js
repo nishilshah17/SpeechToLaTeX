e5fdd597d5a7ff6cfea33e2871152d09286d5fc8
Nishil Shah <nishil.shah17@gmail.com>
2014-09-13 01:56:58 -0400

    bug fixes

--- js/compile.js
+++ js/compile.js
@@ -2,16 +2,16 @@ function compileLaTeX(latex_code) {
 
   //make call to php file
   $.ajax({
-    url: '../tex/createFile.php',
+    url: 'http://ramvellanki.com/s2l/tex/createFile.php',
     data: { 'latex_code': latex_code },
     success: function(data) {
       alert(data);
-    }
+    },
     error: function(error) {
       alert(error);
     }
   });
-  
+
   //embed pdf
   document.getElementById('pdf').innnerHTML = '<iframe src="http://latex.aslushnikov.com/compile?url=https://raw.githubusercontent.com/aslushnikov/latex-online/master/sample/sample.tex" style="width:718px; height:700px;" frameborder="0"></iframe>';
 }
