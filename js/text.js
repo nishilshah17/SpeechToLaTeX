7e43bb64c776f89ef17912f3d1e2227c9af80cf6
RamV13 <ramsai.vellanki@gmail.com>
2014-09-13 03:30:22 -0400

    Input Text Splitting

--- js/text.js
+++ js/text.js
@@ -1,15 +1,37 @@
+function countSpaces(s) {
+    
+    var count = 0;
+    
+    for (var i = 0; i < s.length; i++) {
+        if (s.charAt(i) == " ")
+            count++;
+    }
+    
+    return count;
+    
+}
+
 function convertText(text) {
 
+    text = "x plus y equals three";
+    
+    var words = [];
+
+    for (var i = 0; i < countSpaces(text); i++) {
+        var tmp = [text.substring(0,text.indexOf(" "))]
+        words = words.concat(tmp);
+        text = text.substring(text.indexOf(" ")+1);
+    }
+    
     var convertedText;
 
-    convertedText = "\\documentclass{article}" +
+    convertedText = "\\\documentclass{article}" +
         "\\begin{document}" +
         "\\title{MCLA Concise Review}" +
-        "\\author{Ram Vellanki, Manoaj Kandiakounder, Pranav Marupudi, Jintao Hang}" +
+        "\\\author{Ram Vellanki, Manoaj Kandiakounder, Pranav Marupudi, Jintao Hang}" +
         "\\date{June 2014}" +
-        "\maketitle" +
+        "\\maketitle" +
         "\\tableofcontents" +
-        "%MC CHAPTER 1.................................................... $"+
         "\\newpage" +
         "\\section{Parametrics and Polar Coordinates}" +
         "\\end{document}"
